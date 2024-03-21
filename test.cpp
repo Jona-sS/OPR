2d7a759ad6169b0704b2c8d67396fcbebc2fc912
Jona-sS <Jonas.Seubert@gmx.net>
2024-03-21 08:57:12 +0100

    erstes Progamm

---
+++ test.cpp
@@ -0,0 +1,9 @@
+//erstes Programm
+#include <iostream>
+using namespace std; //sonst std:: vor cout und endl nötig
+
+int main(){
+    int zahl =4;
+    cout << "Hallo " << zahl << endl;
+}
+//Kompelieren mit: g++ test.cpp -o test 
