8caa5fe02e10449301fbfec507c5f9fd86764134
Jona-sS <80956419+Jona-sS@users.noreply.github.com>
2024-04-03 17:53:21 +0200

    Delete test.cpp

--- test.cpp
+++
@@ -1,9 +0,0 @@
-//erstes Programm
-#include <iostream>
-using namespace std; //sonst std:: vor cout und endl nötig
-
-int main(){
-    int zahl =4;
-    cout << "Hallo " << zahl << endl;
-}
-//Kompelieren mit: g++ test.cpp -o test 
